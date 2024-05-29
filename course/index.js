708ac8a09b304b89e9a7b29c6e54d96a123449d7
“[Maya <[mp1964893@gmail.com]>
2024-05-29 18:09:26 +0530

    finalDemo

--- course/index.js
+++ course/index.js
@@ -2,6 +2,8 @@ var a=10;
 var m="maya patil";
 var ko=90.89;
 console.log("maya patil ");
-function add(){
+function add(int ){
     console.log("add function ");
+    console.log(a);
 }
+https://github.com/Maya03Patil/Projects.git
